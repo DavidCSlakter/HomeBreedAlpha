9c6439a3b1fd5e86d1cc93ba86b806364363e92f
DavidCSlakter <davidslakter2@gmail.com>
2019-07-16 19:13:48 -0400

    added new classroom images and made adjustments to some

---
+++ Assets/Scripts/classScene/classSceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classSceneController : MonoBehaviour
+{
+    public Animator LucasAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LucasAnimator.SetBool("isSitting", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
