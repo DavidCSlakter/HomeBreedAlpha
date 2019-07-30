69a106a3a9cb18369cb4a36c445da381a65adf20
DavidCSlakter <davidslakter2@gmail.com>
2019-07-29 23:07:09 -0400

    added menu scene

---
+++ Assets/Scripts/MenuScreenController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MenuScreenController : MonoBehaviour
+{
+    public Animator SceneAnimator;
+
+    public AudioClip Music;
+    public AudioSource Msource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Msource.clip = Music;
+        Msource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)){
+            SceneAnimator.SetTrigger("FadeOut");
+        }
+    }
+}
